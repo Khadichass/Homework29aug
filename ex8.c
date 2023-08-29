caab1d211461ca89919a83ab226eba16bad415dd
Khadicha <246abdurashidova123@gmail.com>
2023-08-29 23:29:34 +0500

    ex8.c done

---
+++ ex8.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int multiplication(int a, int b)
+{
+  int multiplicationn = a * b;
+  return multiplicationn;
+}
+
+void multiplication_1()
+{
+  int a;
+  int b;
+
+  printf("Enter the first number: ");
+  scanf("%d", &a);
+  printf("Enter the second number: ");
+  scanf("%d", &b);
+
+  
+  printf("The sum is %d\n", multiplication(a,b));
+}
+
+int main()
+{
+    multiplication_1();
+    return 0;
+}
