5b51af1bf00527e2ab2212e1dfa1e9f95daefb64
Khadicha <246abdurashidova123@gmail.com>
2023-08-29 23:25:57 +0500

    ex6.c done

---
+++ ex6.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int sum(int a, int b)
+{
+  int summ = a + b;
+  return summ;
+}
+
+void sum_1()
+{
+  int a;
+  int b;
+
+  printf("Enter the first number: ");
+  scanf("%d", &a);
+  printf("Enter the second number: ");
+  scanf("%d", &b);
+
+  
+  printf("The sum is %d\n", sum(a,b));
+}
+
+int main()
+{
+    sum_1();
+    return 0;
+}
